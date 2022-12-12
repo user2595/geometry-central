1ea38fd82d0bf08ea1ccaec1557bc72fbdf3b552
Mark Gillespie <mark.gillespie81@gmail.com>
2022-12-12 10:28:16 -0500

    Run BFF on cut meshes

--- include/geometrycentral/surface/boundary_first_flattening.h
+++ include/geometrycentral/surface/boundary_first_flattening.h
@@ -17,23 +17,35 @@ VertexData<Vector2> parameterizeBFFfromExteriorAngles(ManifoldSurfaceMesh& mesh,
 class BFF {
 public:
   BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_);
+  BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_, const std::vector<Edge>& cut_);
 
   VertexData<Vector2> flatten();
   VertexData<Vector2> flattenFromScaleFactors(const VertexData<double>& uBdy);
   VertexData<Vector2> flattenFromExteriorAngles(const VertexData<double>& kBdy);
   VertexData<Vector2> flattenFromBoth(const Vector<double>& uBdy, const Vector<double>& kBdy);
 
+  CornerData<Vector2> flattenWithCuts();
+  CornerData<Vector2> flattenWithCutsFromScaleFactors(const VertexData<double>& uBdy);
+  CornerData<Vector2> flattenWithCutsFromScaleFactors(const CornerData<double>& uBdy);
+  CornerData<Vector2> flattenWithCutsFromExteriorAngles(const CornerData<double>& kBdy);
+  CornerData<Vector2> flattenWithCutsFromBoth(const Vector<double>& uBdy, const Vector<double>& kBdy);
+
   Vector<double> dirichletToNeumann(const Vector<double>& uBdy);
   Vector<double> neumannToDirichlet(const Vector<double>& kBdy);
 
   std::array<Vector<double>, 2> computeBoundaryPositions(const Vector<double>& uBdy, const Vector<double>& kBdy);
 
+
 protected:
   ManifoldSurfaceMesh& mesh;
   IntrinsicGeometryInterface& geo;
-  VertexData<size_t> vIdx;
-  VertexData<int> iIdx, bIdx;
+  void init();
 
+  //=== Used if there are cuts
+  std::vector<Edge> cut;
+  CornerData<size_t> wIdx;
+  CornerData<int> iIdx, bIdx;
+  size_t nWedges, nInteriorWedges, nBoundaryWedges;
 
   SparseMatrix<double> L, Lii, Lib, Lbb;
   std::unique_ptr<PositiveDefiniteSolver<double>> Liisolver;
@@ -41,12 +53,23 @@ protected:
   Vector<double> Omegai, Omegab;
 
   Vector<bool> isInterior;
-  size_t nInterior, nBoundary, nVertices;
 
   BlockDecompositionResult<double> Ldecomp;
 
   void ensureHaveLSolver();
 };
+
+SparseMatrix<double> constructWedgeCotanLaplacian(ManifoldSurfaceMesh& mesh, IntrinsicGeometryInterface& geom,
+                                                  const CornerData<size_t>& wIdx, size_t nWedges);
+
+// Index wedges on cut mesh. Uncut interior vertices come first, followed by uncut boundary vertices, followed by wedges
+// on the cut
+// Wedges corresponding to uncut interior vertices are called "interior wedges", and all other wedges (on the original
+// boundary or along the cut) are called "boundary wedges"
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut);
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut, size_t* nWedges);
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut, size_t* nWedges,
+                               size_t* nInteriorWedges, size_t* nBoundaryWedges);
 
 } // namespace surface
 } // namespace geometrycentral
--- src/surface/boundary_first_flattening.cpp
+++ src/surface/boundary_first_flattening.cpp
@@ -20,37 +20,37 @@ VertexData<Vector2> parameterizeBFFfromExteriorAngles(ManifoldSurfaceMesh& mesh,
   return bff.flattenFromExteriorAngles(exteriorAngles);
 }
 
-BFF::BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_) : mesh(mesh_), geo(geo_) {
+BFF::BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_) : mesh(mesh_), geo(geo_) { init(); }
 
-  GC_SAFETY_ASSERT(mesh.eulerCharacteristic() == 2 && mesh.nBoundaryLoops() == 1,
-                   "Input to BFF must be a topological disk");
+BFF::BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_, const std::vector<Edge>& cut_)
+    : mesh(mesh_), geo(geo_), cut(cut_) {
+  init();
+}
 
-  vIdx = mesh.getVertexIndices();
-  iIdx = VertexData<int>(mesh, -1);
-  bIdx = VertexData<int>(mesh, -1);
+void BFF::init() {
 
-  geo.requireCotanLaplacian();
+  wIdx = indexWedges(mesh, cut, &nWedges, &nInteriorWedges, &nBoundaryWedges);
 
-  L = geo.cotanLaplacian;
+  // GC_SAFETY_ASSERT(mesh.eulerCharacteristic() == 2 && mesh.nBoundaryLoops() == 1,
+  //                  "Input to BFF must be a topological disk");
 
-  isInterior = Vector<bool>(mesh.nVertices());
-  size_t iB = 0, iI = 0;
-  for (Vertex v : mesh.vertices()) {
-    if (v.isBoundary()) {
-      bIdx[v] = iB++;
-      isInterior(vIdx[v]) = false;
+  iIdx = CornerData<int>(mesh, -1);
+  bIdx = CornerData<int>(mesh, -1);
+
+  isInterior = Vector<bool>(nWedges);
+  for (Corner c : mesh.corners()) {
+    size_t iW = wIdx[c];
+    if (iW < nInteriorWedges) {
+      iIdx[c] = iW;
+      isInterior(iW) = true;
     } else {
-      iIdx[v] = iI++;
-      isInterior(vIdx[v]) = true;
+      bIdx[c] = iW - nInteriorWedges;
+      isInterior(iW) = false;
     }
   }
-  nVertices = mesh.nVertices();
-  nInterior = mesh.nInteriorVertices();
-  nBoundary = nVertices - nInterior;
 
-  geo.requireCotanLaplacian();
-  SparseMatrix<double> L = geo.cotanLaplacian;
-  shiftDiagonal(L, 1e-12);
+  L = constructWedgeCotanLaplacian(mesh, geo, wIdx, nWedges);
+  shiftDiagonal(L, 1e-8);
 
   Ldecomp = blockDecomposeSquare(L, isInterior);
 
@@ -61,16 +61,97 @@ BFF::BFF(ManifoldSurfaceMesh& mesh_, IntrinsicGeometryInterface& geo_) : mesh(me
   // TODO: extract this factorization from a full factorization of L
   Liisolver.reset(new PositiveDefiniteSolver<double>(Lii));
 
-  geo.requireVertexAngleSums();
-  Omegai = Vector<double>(nInterior);
-  Omegab = Vector<double>(nBoundary);
-  for (Vertex v : mesh.vertices()) {
-    if (v.isBoundary()) {
-      Omegab(bIdx[v]) = M_PI - geo.vertexAngleSums[v];
+  Omegai = Vector<double>::Constant(nInteriorWedges, 2 * M_PI);
+  Omegab = Vector<double>::Constant(nBoundaryWedges, M_PI);
+  geo.requireCornerAngles();
+  for (Corner c : mesh.corners()) {
+    size_t iW = wIdx[c];
+    if (isInterior(iW)) {
+      Omegai(iIdx[c]) -= geo.cornerAngles[c];
     } else {
-      Omegai(iIdx[v]) = 2 * M_PI - geo.vertexAngleSums[v];
+      Omegab(bIdx[c]) -= geo.cornerAngles[c];
     }
   }
+  geo.unrequireCornerAngles();
+}
+
+CornerData<Vector2> BFF::flattenWithCuts() {
+  // Impose 0 scaling on boundary
+  Vector<double> uBdy = Vector<double>::Zero(nBoundaryWedges);
+
+  // Compute complementary data
+  Vector<double> kBdy = dirichletToNeumann(uBdy);
+
+  // Flatten
+  return flattenWithCutsFromBoth(uBdy, kBdy);
+}
+
+CornerData<Vector2> BFF::flattenWithCutsFromScaleFactors(const VertexData<double>& uData) {
+  // Extract boundary values
+  Vector<double> uBdy(nBoundaryWedges);
+  for (Corner c : mesh.corners()) {
+    if (!isInterior(bIdx[c])) uBdy(bIdx[c]) = uData[c.vertex()];
+  }
+
+  // Compute complementary data
+  Vector<double> kBdy = dirichletToNeumann(uBdy);
+
+  // Flatten
+  return flattenWithCutsFromBoth(uBdy, kBdy);
+}
+
+CornerData<Vector2> BFF::flattenWithCutsFromScaleFactors(const CornerData<double>& uData) {
+  // Extract boundary values
+  Vector<double> uBdy(nBoundaryWedges);
+  for (Corner c : mesh.corners()) {
+    if (!isInterior(bIdx[c])) uBdy(bIdx[c]) = uData[c];
+  }
+
+  // Compute complementary data
+  Vector<double> kBdy = dirichletToNeumann(uBdy);
+
+  // Flatten
+  return flattenWithCutsFromBoth(uBdy, kBdy);
+}
+
+CornerData<Vector2> BFF::flattenWithCutsFromExteriorAngles(const CornerData<double>& kData) {
+  // Extract boundary values
+  Vector<double> kBdy(nBoundaryWedges);
+  for (Corner c : mesh.corners()) {
+    if (bIdx[c] >= 0) kBdy(bIdx[c]) = kData[c];
+  }
+
+  GC_SAFETY_ASSERT(abs(kBdy.sum() - 2 * M_PI) < 1e-3,
+                   "BFF error: target exterior angles must sum to 2 pi, but the input sums to " +
+                       std::to_string(kBdy.sum()));
+
+  // Compute complementary data
+  Vector<double> uBdy = neumannToDirichlet(kBdy);
+
+  // FlattenWithCuts
+  return flattenWithCutsFromBoth(uBdy, kBdy);
+}
+
+CornerData<Vector2> BFF::flattenWithCutsFromBoth(const Vector<double>& uBdy, const Vector<double>& kBdy) {
+
+  Vector<double> boundaryX, boundaryY;
+  std::tie(boundaryX, boundaryY) = tuple_cat(computeBoundaryPositions(uBdy, kBdy));
+
+  Vector<double> interiorX = Liisolver->solve(-Lib * boundaryX);
+  Vector<double> interiorY = Liisolver->solve(-Lib * boundaryY);
+
+  CornerData<Vector2> parm(mesh);
+  for (Corner c : mesh.corners()) {
+    if (isInterior(wIdx[c])) {
+      size_t iW = iIdx[c];
+      parm[c] = Vector2{interiorX(iW), interiorY(iW)};
+    } else {
+      size_t iW = bIdx[c];
+      parm[c] = Vector2{boundaryX(iW), boundaryY(iW)};
+    }
+  }
+
+  return parm;
 }
 
 VertexData<Vector2> BFF::flatten() {
@@ -107,6 +188,8 @@ VertexData<Vector2> BFF::flattenFromExteriorAngles(const VertexData<double>& kDa
 }
 
 VertexData<Vector2> BFF::flattenFromBoth(const Vector<double>& uBdy, const Vector<double>& kBdy) {
+  GC_SAFETY_ASSERT(cut.empty(), "BFF can only generate VertexData textures if there are no cuts. To parameterize cut "
+                                "meshes, use BFF::flattenWithCuts");
 
   Vector<double> boundaryX, boundaryY;
   std::tie(boundaryX, boundaryY) = tuple_cat(computeBoundaryPositions(uBdy, kBdy));
@@ -116,12 +199,12 @@ VertexData<Vector2> BFF::flattenFromBoth(const Vector<double>& uBdy, const Vecto
 
   VertexData<Vector2> parm(mesh);
   for (Vertex v : mesh.vertices()) {
-    if (v.isBoundary()) {
-      size_t iV = bIdx[v];
-      parm[v] = Vector2{boundaryX(iV), boundaryY(iV)};
+    if (isInterior(wIdx[v.corner()])) {
+      size_t iW = iIdx[v.corner()];
+      parm[v] = Vector2{interiorX(iW), interiorY(iW)};
     } else {
-      size_t iV = iIdx[v];
-      parm[v] = Vector2{interiorX(iV), interiorY(iV)};
+      size_t iW = bIdx[v.corner()];
+      parm[v] = Vector2{boundaryX(iW), boundaryY(iW)};
     }
   }
 
@@ -146,52 +229,83 @@ Vector<double> BFF::neumannToDirichlet(const Vector<double>& kBdy) {
 
 std::array<Vector<double>, 2> BFF::computeBoundaryPositions(const Vector<double>& uBdy, const Vector<double>& kBdy) {
 
-  auto src = [](Edge e) { return e.halfedge().vertex(); };
-  auto dst = [](Edge e) { return e.halfedge().next().vertex(); };
-
   geo.requireEdgeLengths();
 
   double phi = 0;
 
-  std::vector<Eigen::Triplet<double>> Ntriplets;
-  Vector<double> targetLength(nBoundary);
-  DenseMatrix<double> T(2, nBoundary);
+  // Iterate over boundary like here:
+  // https://github.com/GeometryCollective/boundary-first-flattening/blob/master/mesh/include/CutIterator.inl#L14
+  // We assume that the boundary has one connected component
 
-  for (BoundaryLoop b : mesh.boundaryLoops()) {
-    for (Edge e : b.adjacentEdges()) {
-      int iV = bIdx[src(e)];
-      GC_SAFETY_ASSERT(0 <= iV && iV < (int)nBoundary, "invalid boundary vertex index");
-
-      targetLength(iV) = geo.edgeLengths[e] * exp(0.5 * (uBdy(bIdx[src(e)]) + uBdy(bIdx[dst(e)])));
-
-      T(0, iV) = cos(phi);
-      T(1, iV) = sin(phi);
-
-      Ntriplets.emplace_back(iV, iV, geo.edgeLengths[e]);
-
-      phi += kBdy(bIdx[dst(e)]);
-    }
+  EdgeData<bool> onCut;
+  if (!cut.empty()) {
+    onCut = EdgeData<bool>(mesh, false);
+    for (Edge e : cut) onCut[e] = true;
   }
 
-  SparseMatrix<double> Ninv(nBoundary, nBoundary);
+  auto onCutBoundary = [&](Halfedge he) -> bool {
+    return he.edge().isBoundary() || (!cut.empty() && onCut[he.edge()]);
+  };
+
+  Halfedge firstHe = (!cut.empty()) ? cut[0].halfedge() : mesh.boundaryLoop(0).halfedge().twin();
+  GC_SAFETY_ASSERT(firstHe.isInterior(), "???");
+  GC_SAFETY_ASSERT(onCutBoundary(firstHe), "Not on cut???");
+
+  auto nextAlongCut = [&](Halfedge curr) -> Halfedge {
+    // if (cut.empty()) return curr.twin().next().twin();
+    Halfedge he = curr.next();
+    while (!onCutBoundary(he)) {
+      he = he.twin().next(); // loop around one ring clockwise
+      if (he == curr.next()) break;
+    }
+    if (!onCutBoundary(he)) {
+      std::cout << "??? can't find cut?" << std::endl;
+    }
+    return he;
+  };
+
+  std::vector<Eigen::Triplet<double>> Ntriplets;
+  Vector<double> targetLength(nBoundaryWedges);
+  DenseMatrix<double> T(2, nBoundaryWedges);
+
+  Halfedge curr = firstHe;
+  do {
+    int iW = bIdx[curr.corner()];
+    GC_SAFETY_ASSERT(iW >= 0, "invalid boundary vertex index");
+
+    targetLength(iW) =
+        geo.edgeLengths[curr.edge()] * exp(0.5 * (uBdy(bIdx[curr.corner()]) + uBdy(bIdx[curr.next().corner()])));
+
+    T(0, iW) = cos(phi);
+    T(1, iW) = sin(phi);
+
+    Ntriplets.emplace_back(iW, iW, geo.edgeLengths[curr.edge()]);
+
+    phi += kBdy(bIdx[curr.next().corner()]);
+
+    curr = nextAlongCut(curr);
+  } while (curr != firstHe);
+
+  SparseMatrix<double> Ninv(nBoundaryWedges, nBoundaryWedges);
   Ninv.setFromTriplets(std::begin(Ntriplets), std::end(Ntriplets));
 
   Vector<double> roundedLength =
       targetLength - Ninv * T.transpose() * (T * Ninv * T.transpose()).inverse() * T * targetLength;
 
-  std::array<Vector<double>, 2> bdyPositions{Vector<double>(nBoundary), Vector<double>(nBoundary)};
+  std::array<Vector<double>, 2> bdyPositions{Vector<double>(nBoundaryWedges), Vector<double>(nBoundaryWedges)};
   double x = 0, y = 0;
-  for (BoundaryLoop b : mesh.boundaryLoops()) {
-    for (Edge e : b.adjacentEdges()) {
-      size_t iV = bIdx[src(e)];
+  curr = firstHe;
+  do {
+    size_t iW = bIdx[curr.corner()];
 
-      bdyPositions[0](iV) = x;
-      bdyPositions[1](iV) = y;
+    bdyPositions[0](iW) = x;
+    bdyPositions[1](iW) = y;
 
-      x += roundedLength(iV) * T(0, iV);
-      y += roundedLength(iV) * T(1, iV);
-    }
-  }
+    x += roundedLength(iW) * T(0, iW);
+    y += roundedLength(iW) * T(1, iW);
+
+    curr = nextAlongCut(curr);
+  } while (curr != firstHe);
 
   return bdyPositions;
 }
@@ -201,5 +315,88 @@ void BFF::ensureHaveLSolver() {
     Lsolver.reset(new PositiveDefiniteSolver<double>(L));
   }
 }
+
+SparseMatrix<double> constructWedgeCotanLaplacian(ManifoldSurfaceMesh& mesh, IntrinsicGeometryInterface& geo,
+                                                  const CornerData<size_t>& wIdx, size_t nWedges) {
+  std::vector<Eigen::Triplet<double>> T;
+
+  geo.requireHalfedgeCotanWeights();
+  for (Halfedge ij : mesh.interiorHalfedges()) {
+    size_t i = wIdx[ij.corner()];
+    size_t j = wIdx[ij.next().corner()];
+    double weight = geo.halfedgeCotanWeights[ij];
+
+    T.emplace_back(i, i, weight);
+    T.emplace_back(i, j, -weight);
+    T.emplace_back(j, i, -weight);
+    T.emplace_back(j, j, weight);
+  }
+  geo.unrequireHalfedgeCotanWeights();
+
+  SparseMatrix<double> L(nWedges, nWedges);
+  L.setFromTriplets(T.begin(), T.end());
+
+  return L;
+}
+
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut) {
+  size_t nWedges, nInteriorWedges, nBoundaryWedges;
+  return indexWedges(mesh, cut, &nWedges, &nInteriorWedges, &nBoundaryWedges);
+}
+
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut, size_t* nWedges) {
+  size_t nInteriorWedges, nBoundaryWedges;
+  return indexWedges(mesh, cut, nWedges, &nInteriorWedges, &nBoundaryWedges);
+}
+
+// https://github.com/GeometryCollective/boundary-first-flattening/blob/master/project/src/Bff.cpp#L755
+CornerData<size_t> indexWedges(ManifoldSurfaceMesh& mesh, const std::vector<Edge>& cut, size_t* nWedges,
+                               size_t* nInteriorWedges, size_t* nBoundaryWedges) {
+  VertexData<bool> vertexOnCut(mesh, false);
+  EdgeData<bool> edgeOnCut(mesh, false);
+  for (Edge e : cut) {
+    vertexOnCut[e.halfedge().tailVertex()] = true;
+    vertexOnCut[e.halfedge().tipVertex()] = true;
+    edgeOnCut[e] = true;
+  }
+
+  size_t iW = 0;
+  CornerData<size_t> wedgeIndices(mesh);
+
+  // Index true interior vertices
+  for (Vertex v : mesh.vertices()) {
+    if (!v.isBoundary() && !vertexOnCut[v]) {
+      for (Corner c : v.adjacentCorners()) wedgeIndices[c] = iW;
+      iW++;
+    }
+  }
+  *nInteriorWedges = iW;
+
+  // Index boundary (but uncut) vertices
+  for (Vertex v : mesh.vertices()) {
+    if (v.isBoundary() && !vertexOnCut[v]) {
+      for (Corner c : v.adjacentCorners()) wedgeIndices[c] = iW;
+      iW++;
+    }
+  }
+
+  // Index vertices on cut
+  for (Edge e : cut) {
+    for (Halfedge he : e.adjacentHalfedges()) {
+      Halfedge curr = he;
+      do {
+        curr = curr.twin().next();
+        wedgeIndices[curr.corner()] = iW;
+      } while (!edgeOnCut[curr.edge()]);
+      iW++;
+    }
+  }
+
+  *nWedges = iW;
+  *nBoundaryWedges = *nWedges - *nInteriorWedges;
+
+  return wedgeIndices;
+}
+
 } // namespace surface
 } // namespace geometrycentral
